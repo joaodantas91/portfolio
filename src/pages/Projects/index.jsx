3c300db60caab18d8cde057ff8bd42dfc508afad
João Dantas <joaostadantas@gmail.com>
2021-02-16 14:10:53 -0300

    projects page finished

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,10 +1,68 @@
 import React from 'react';
+import Container from '../../components/Container/styles';
+import {Wrapper} from './styles'
+import { Scrollbars } from 'react-custom-scrollbars';
+
+// Frame
+import Chrome from './img/chrome.svg';
+
+// Projects
+import Alubar from './img/alubar.png';
+import Aumed from './img/aumed.png';
+import Dsm from './img/dsm.png';
+import Eurolubs from './img/eurolubs.png';
+import Ihebe from './img/ihebe.png';
+import Medicalcenter from './img/medical-center.png';
+import Pds from './img/pds.png';
 
 function Projects() {
   return (
-    <>
-      <h2>Teste</h2>
-    </>
+    <Container>
+      <Scrollbars>
+        <Wrapper>
+        <h2>Projetos</h2>
+          <div className="frames">
+            <a href="http://www.portodiassaude.com.br/" target="_blank" rel="noreferrer" className="frame">
+              <img src={Chrome} className="frame-img" alt="frame" />
+              <img src={Pds} className="project" alt="frame" />
+            </a>
+
+            <a href="http://alubar2020.sitebeta.com.br/" target="_blank" rel="noreferrer" className="frame">
+              <img src={Chrome} className="frame-img" alt="frame"/>
+              <img src={Alubar} className="project" alt="frame" />
+            </a>
+
+            <a href="http://eurolubs.sitebeta.com.br/" target="_blank" rel="noreferrer" className="frame">
+              <img src={Chrome} className="frame-img" alt="frame"/>
+              <img src={Eurolubs} className="project" alt="frame" />
+            </a>
+
+            <a href="http://medicalcenter.sitebeta.com.br/" target="_blank" rel="noreferrer" className="frame">
+              <img src={Chrome} className="frame-img" alt="frame"/>
+              <img src={Medicalcenter} className="project" alt="frame" />
+            </a>
+
+
+            <a href="http://ihebe.sitebeta.com.br/" target="_blank" rel="noreferrer" className="frame">
+              <img src={Chrome} className="frame-img" alt="frame"/>
+              <img src={Ihebe} className="project" alt="frame" />
+            </a>
+
+
+            <a href="http://www.dsmpa.com.br/  " target="_blank" rel="noreferrer" className="frame">
+              <img src={Chrome} className="frame-img" alt="frame"/>
+              <img src={Dsm} className="project" alt="frame" />
+            </a>
+
+            <a href="http://aumed.sitebeta.com.br/" target="_blank" rel="noreferrer" className="frame">
+              <img src={Chrome} className="frame-img" alt="frame"/>
+              <img src={Aumed} className="project" alt="frame" />
+            </a>
+
+          </div>
+        </Wrapper>
+      </Scrollbars>
+    </Container>
   )
 }
 
---
+++ src/pages/Projects/styles.jsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 1110px;
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 25px 10px;
+  h2 {
+    text-align: center;
+    margin: 0;
+    font-size: 50px;
+    font-weight: 600;
+    line-height: .9;
+    margin-bottom: 40px;
+  }
+  .frames {
+    display: flex;
+    flex-wrap: wrap;
+    .frame {
+      height: fit-content;
+      display: flex;
+      flex-direction: column;
+      width: calc(33.334% - 40px);
+      margin: 0 20px;
+      margin-bottom: 20px;
+      box-shadow: 0 0px 6px rgba(0,0,0,.2);
+      
+      .frame-img {
+        width: 130px;
+        max-width: 100%;
+        margin: 5px 0 5px 5px;
+      }
+      .project {
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+  @media (max-width: 992px) {
+    .frames {
+      .frame {
+        width: calc(50% - 40px);
+      }
+    }
+  }
+
+  @media (max-width: 576px) {
+    .frames {
+      .frame {
+        width: calc(100% - 40px);
+      }
+    }
+  }
+`;
